590bd35cf92ed9303fb77b111827eb640a2e217f
Megood <wxpee30@gmail.com>
2025-02-08 23:17:24 +0100

    lyrics in place now

--- data/events/PA Lyrics.hx
+++ data/events/PA Lyrics.hx
@@ -7,12 +7,12 @@ public var coolassshader = new CustomShader("NewGlitch2");
 function create() {
     for (event in events) {
         if (event.name == 'PA Lyrics') {
-            dalyricsa = new FlxText(0, 165, 1200, "", 70);
+            dalyricsa = new FlxText(0, 600, 1200, "", 70); // Default position at 0,0
             dalyricsa.setFormat(Paths.font("MPLUSRounded1c-Black.ttf"), 40, 0xFF5883B0, "center", FlxTextBorderStyle.OUTLINE_FAST, 0xFFFFFFFF);
-            dalyricsa.scrollFactor.set(1500,200);
+            dalyricsa.scrollFactor.set();
             dalyricsa.borderSize = 2.5;
             dalyricsa.borderQuality = 1;
-            dalyricsa.screenCenter(FlxAxes.X);
+	    dalyricsa.screenCenter(FlxAxes.X);
             add(dalyricsa);
             dalyricsa.cameras = [camGame];
             break;
@@ -30,6 +30,10 @@ function onEvent(e) {
         dalyricsa.text = params[1];
     }
     
+    // Add position handling (params[5] for X, params[6] for Y)
+    if (params[5] != null) dalyricsa.x = Std.parseFloat(params[5]);
+    if (params[6] != null) dalyricsa.y = Std.parseFloat(params[6]);
+    
     enableit = params[3] == true;
     hideit = params[4] == true;
 }
@@ -41,4 +45,6 @@ function stepHit(step:Int) {
 
 function postUpdate(elapsed) {
     dalyricsa.shader = enableit ? coolassshader : null;
+    if (hideit) dalyricsa.visible = false;
+    else dalyricsa.visible = true;
 }
