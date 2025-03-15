8d413856a50b6644b9463bf2d1520af1222d99de
Megood <wxpee30@gmail.com>
2025-03-15 19:26:23 +0100

    cutscene pauses and resumes and etc

--- songs/my side/scripts/Cutscene.hx
+++ songs/my side/scripts/Cutscene.hx
@@ -1,3 +1,4 @@
+import funkin.backend.scripting.events.DrawEvent;
 import hxvlc.flixel.FlxVideoSprite;
 
 var cutscene = new FlxVideoSprite(0, 0);
@@ -11,7 +12,27 @@ function postCreate() {
 	add(cutscene);
 	cutscene.play();
 	cutscene.cameras = [camHUD];
+	if (FlxG.signals.focusGained.has(cutscene.resume)) {
+        FlxG.signals.focusGained.remove(cutscene.resume);
+    }
 
+}
+
+
+function onGamePause() {
+	cutscene.pause();
+}
+
+function onSubstateClose() {
+    if (paused) {
+        cutscene.resume();
+    }
+}
+
+function onFocus() {
+    if (!paused) {
+    	cutscene.resume();
+    }
 }
 
 function stepHit(curStep:Int) {
